a45f49da816e2423453bbe64abcbea1c3fa87091
Jimmy Chen <jchen.tc@gmail.com>
2023-06-28 23:13:35 +1000

    Update scripts to query latest bootnodes

---
+++ lighthouse/lighthouse.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+boot_nodes=$(curl -s https://config.4844-devnet-6.ethpandaops.io/api/v1/nodes/inventory | jq '[ .ethereum_pairs[] | .consensus.enr ] | join(",")' | tr -d '"')
+
+exec lighthouse bn \
+  --datadir /data \
+  --execution-jwt /config/jwtsecret \
+  --execution-endpoint http://geth:8551 \
+  --self-limiter=blob_sidecars_by_range:512/10 \
+  --debug-level debug \
+  --testnet-dir /config/testnet \
+  --http \
+  --http-address=0.0.0.0 \
+  --metrics \
+  --metrics-address=0.0.0.0 \
+  --port ${CL_P2P_PORT:-9000} \
+  --boot-nodes "$boot_nodes"
