0b45b2f48324a6ea1527ab019da0d667dc02325e
Pablo Castellano <pablo@anche.no>
2023-10-25 20:08:18 +0200

    Remove enodes/enr data downloads in geth.sh and lighthouse.sh scripts

--- update_files.sh
+++ update_files.sh
@@ -7,4 +7,5 @@ curl -s $BASE_URL/cl/config.yaml > cl_config/config.yaml
 curl -s $BASE_URL/api/v1/nodes/inventory | jq -r '.ethereum_pairs[] | .consensus.enr' | sed 's/^/- /' > cl_config/boot_enr.yaml
 curl -s $BASE_URL/cl/genesis.ssz > cl_config/genesis.ssz
 
+curl -s $BASE_URL/api/v1/nodes/inventory | jq -r '[ .ethereum_pairs[] | .execution.enode ] | join(",")'  | tr -d '"' > geth/enodes.list
 curl -s $BASE_URL/el/genesis.json > geth/genesis.json
